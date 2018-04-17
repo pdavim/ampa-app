e22b2d1aed378291bc0e1bacdb10968d2bc07a22
Pedro Davim <pdavimmilkman@gmail.com>
2018-04-17 03:15:26 +0100

    Update add_model_form.js

--- src/views/views_js/add_model_form.js
+++ src/views/views_js/add_model_form.js
@@ -1,11 +1,11 @@
    // Initialize Firebase
    var config = {
-       apiKey: "AIzaSyCM8s8LG1maY7lZC8sDPft5n7gOaTpd5P8",
-       authDomain: "productionapp-96303.firebaseapp.com",
-       databaseURL: "https://productionapp-96303.firebaseio.com",
-       projectId: "productionapp-96303",
-       storageBucket: "productionapp-96303.appspot.com",
-       messagingSenderId: "488585576112"
+       apiKey: "",
+       authDomain: ".firebaseapp.com",
+       databaseURL: "https://.firebaseio.com",
+       projectId: "",
+       storageBucket: "",
+       messagingSenderId: ""
    };
    firebase.initializeApp(config);
 
